ca69db1a7b7cacfe4162293d29e3d8adbec2b110
Johanny LS <johanny.dls@gmail.com>
2019-06-13 11:49:46 -0300

    Add structure for Feed component

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,11 +1,73 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 //Componente em formato de classe
 
 class Feed extends Component {
     render() {
         return (
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Johanny LS</span>
+                            <span className="place">Campina Grande</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/profile_500.jpg" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="Like" />
+                            <img src={comment} alt="Comment" />
+                            <img src={send} alt="Send" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Uma foto legal
+                            <span>#casamento #diziaejohanny</span>
+                        </p>
+                    </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Johanny LS</span>
+                            <span className="place">Campina Grande</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/profile_500.jpg" />
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="Like" />
+                            <img src={comment} alt="Comment" />
+                            <img src={send} alt="Send" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Uma foto legal
+                            <span>#casamento #diziaejohanny</span>
+                        </p>
+                    </footer>
+                </article>
+
+            </section>
         );
     }
 }
