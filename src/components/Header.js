da0bc9f0d4bf83273a84cf88d4fa57359b7802ee
Johanny LS <johanny.dls@gmail.com>
2019-06-13 11:22:57 -0300

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Header.css';
+
+//Component em formato de função
+
+export default function Header() {
+  return (
+    <header id="main-header">
+        <div className="header-content">
+            <img src="" alt="InstaRocket" />
+            <img src="" alt="Enviar publicação" />
+        </div>
+    </header>
+  );
+}
